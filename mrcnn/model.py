1fdcc1ee7d4f217d1bfafea42f1bd5606057da70
DarkVortex <arnav98@gmail.com>
2022-07-17 22:24:22 -0400

    fixed TF update restructure errors

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2102,14 +2102,7 @@ class MaskRCNN():
         exclude: list of layer names to exclude
         """
         import h5py
-        # Conditional import to support versions of Keras before 2.2
-        # TODO: remove in about 6 months (end of 2018)
-        try:
-            from keras.engine import saving
-        except ImportError:
-            # Keras before 2.2 used the 'topology' namespace.
-            from keras.engine import topology as saving
-
+        from tensorflow.python.keras.saving import hdf5_format
         if exclude:
             by_name = True
 
@@ -2130,9 +2123,9 @@ class MaskRCNN():
             layers = filter(lambda l: l.name not in exclude, layers)
 
         if by_name:
-            saving.load_weights_from_hdf5_group_by_name(f, layers)
+            hdf5_format.load_weights_from_hdf5_group_by_name(f, layers)
         else:
-            saving.load_weights_from_hdf5_group(f, layers)
+            hdf5_format.load_weights_from_hdf5_group(f, layers)
         if hasattr(f, 'close'):
             f.close()
 
